f39fe2cbb2d05b8134036d5d0b3e6503d03fd5f8
Darius3001 <darius.mertin@outlook.com>
2024-05-13 23:39:15 +0200

    added nvim themes

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,3 +1,16 @@
 return {
     { "Mofiqul/dracula.nvim" },
+    {
+        "vermdeep/darcula_dark.nvim",
+        lazy = false,
+        priority = 1002,
+    },
+    {
+        "rktjmp/lush.nvim",
+        lazy = false,
+        priority = 1000,
+        dependencies = {
+            "vermdeep/darcula_dark.nvim",
+        },
+    },
 }
