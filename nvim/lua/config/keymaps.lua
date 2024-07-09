6119b774531e95e8bc570d8690a7490fea862076
Darius3001 <darius.mertin@outlook.com>
2024-07-09 17:08:42 +0200

    added cool custom vim motions

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,3 +9,27 @@ for i = 1, 9 do
   vim.api.nvim_set_keymap("n", "<C-" .. i .. ">", "<cmd>BufferLineGoToBuffer " .. i .. "<CR>", { noremap = true, silent = true })
 end
 
+function move_text_up()
+  vim.cmd("'<,'>m '<-2")
+  vim.cmd("normal! gv")
+end
+
+function move_text_down()
+  vim.cmd("'<,'>m '>+1")
+  vim.cmd("normal! gv")
+end
+
+function move_text_right()
+  vim.cmd("'<,'>normal! >>")
+  vim.cmd("normal! gv")
+end
+
+function move_text_left()
+  vim.cmd("'<,'>normal! <<")
+  vim.cmd("normal! gv")
+end
+
+vim.api.nvim_set_keymap('v', '<S-Up>', ":lua move_text_up()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<S-Down>', ":lua move_text_down()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<S-Right>', ":lua move_text_right()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<S-Left>', ":lua move_text_left()<CR>", { noremap = true, silent = true })
