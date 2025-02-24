f50dfe4fbfad3c434da230f18be79836e2ff63a0
Darius3001 <darius.mertin@outlook.com>
2025-02-24 23:56:04 +0100

    replaced by packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,35 @@
-require("config.lazy")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<Leader>e', '<cmd>Explore<cr>')
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+  end
+end
+
+ensure_packer()
+
+local packer = require("packer")
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+
+  packer.install()
+end)
+
+require("lspconfig").lua_ls.setup({})
+
+require("mason").setup({})
+
+--{ "neovim/nvim-lspconfig" },
+--{ "williamboman/mason.nvim" },
