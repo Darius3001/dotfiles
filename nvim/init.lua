2c7f45f216a1d37e805bf93d87db45e633e95e1a
Darius3001 <darius.mertin@outlook.com>
2025-02-26 20:27:50 +0100

    more language setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,10 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.wo.relativenumber = true
+
 vim.keymap.set('n', '<Leader>e', '<cmd>Explore<cr>')
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
 
 local ensure_lazy = function()
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -31,10 +34,22 @@ require("lazy").setup({
   spec = {
     { "neovim/nvim-lspconfig" },
     { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
   }
 })
 
-require("lspconfig").lua_ls.setup({})
+local lspconfig = require("lspconfig")
 
-require("mason").setup({})
+require("mason").setup()
 
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls" },
+  handlers = {
+    function(server_name)
+      lspconfig[server_name].setup({})
+    end
+  }
+})
+
+require('nvim-treesitter.install').ensure_installed = "all"
