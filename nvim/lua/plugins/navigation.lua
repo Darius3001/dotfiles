ab6470274c6cfcce49a7c7b0456c4ed0a03c5256
Darius3001 <darius.mertin@outlook.com>
2025-03-03 10:52:38 +0100

    fix

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -12,7 +12,7 @@ return {
         },
         defaults = {
           file_ignore_patterns = {
-            "node_modules", "build", ".git"
+            "node_modules/", "build/", "%.git/"
           },
         }
       }
