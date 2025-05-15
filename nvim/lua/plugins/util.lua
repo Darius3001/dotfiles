adbbcfb0ac377217a506b9b240cf23df40bb43f1
Darius3001 <darius.mertin@outlook.com>
2025-05-15 23:59:34 +0200

    primeagen refactor plugin

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -42,4 +42,13 @@ return {
       })
     end
   },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    lazy = false,
+    opts = {},
+  },
 }
