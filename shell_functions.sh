027292976aeb28652461cf679a74513a6c29cfc0
Darius Mertin <darius.mertin@sap.com>
2025-06-17 14:16:38 +0200

    WIP

--- shell_functions.sh
+++ shell_functions.sh
@@ -4,10 +4,10 @@ alias lg="lazygit"
 alias ld="lazydocker"
 alias v="nvim"
 alias agrep="alias | grep"
+alias w="cd $(walk)"
 
 alias kg="kubectl get"
 alias kd="kubectl describe"
 
 alias kgwf="kubectl get workflowtemplate"
 alias kdwf="kubectl describe workflowtemplate"
-#alias xc="xclip -selection clipboard"
