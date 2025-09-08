bf215c1a2af2d9f375b3789a08687ab17da748d1
Darius <darius.mertin@outlook.com>
2025-09-08 18:51:02 +0200

    removed comments

--- nix/hosts/laptop/configuration.nix
+++ nix/hosts/laptop/configuration.nix
@@ -7,26 +7,6 @@
   networking.hostName = "darius-nixos";
   networking.networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   system.stateVersion = "25.05";
 
 }
