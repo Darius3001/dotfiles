1cdb5faea635d8d85c0aed960ec9b714b74f2ed9
Darius <darius.mertin@outlook.com>
2025-10-26 18:12:58 +0100

    wip

--- nix/home.nix
+++ nix/home.nix
@@ -1,64 +1,68 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "darius";
-  home.homeDirectory = "/home/darius";
-  home.stateVersion = "25.05";
-
   programs.home-manager.enable = true;
 
-  home.file.".config/nvim" = {
-    source = ./../nvim;
-    recursive = true;
-  };
+  home = {
+    username = "darius";
+    homeDirectory = "/home/darius";
+    stateVersion = "25.05";
 
-  home.file.".config/polybar" = {
-    source = ./../polybar;
-    recursive = true;
-  };
+    file = {
+      ".config/nvim" = {
+        source = ./../nvim;
+        recursive = true;
+      };
 
-  home.file.".config/rofi" = {
-    source = ./../rofi;
-    recursive = true;
-  };
+      ".config/polybar" = {
+        source = ./../polybar;
+        recursive = true;
+      };
 
-  home.file.".tmux.conf" = {
-    source = ./../.tmux.conf;
-  };
+      ".config/rofi" = {
+        source = ./../rofi;
+        recursive = true;
+      };
 
-  home.file.".tmux/plugins/tpm" = {
-    source = pkgs.fetchFromGitHub {
-      owner = "tmux-plugins";
-      repo = "tpm";
-      rev = "v3.1.0";
-      sha256 = "sha256-CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
+      ".tmux.conf" = {
+        source = ./../.tmux.conf;
+      };
+
+      ".tmux/plugins/tpm" = {
+        source = pkgs.fetchFromGitHub {
+          owner = "tmux-plugins";
+          repo = "tpm";
+          rev = "v3.1.0";
+          sha256 = "sha256-CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
+        };
+        recursive = true;
+      };
     };
-    recursive = true;
+
+    packages = with pkgs; [
+      ripgrep
+      fd
+      tmux
+      neovim
+      lazygit
+      lazydocker
+      fzf
+      btop
+
+      gcc
+      cargo
+      python313
+      nodejs_24
+
+      xclip
+      xkb-switch
+      brightnessctl
+      pamixer
+      polybar
+      rofi
+      rofi-power-menu
+
+      kitty
+    ];
   };
-
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    tmux
-    neovim
-    lazygit
-    lazydocker
-    fzf
-    btop
-
-    gcc
-    cargo
-    python313
-    nodejs_24
-
-    xclip
-    xkb-switch
-    brightnessctl
-    pamixer
-    polybar
-    rofi
-    rofi-power-menu
-
-    kitty
-  ];
 }
