659b4ad79f085929463386c11a68bc04573111ac
Darius Mertin <darius.mertin@sap.com>
2025-12-02 13:55:29 +0100

    nvimlint

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,89 +1,92 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = false,
-        underline = true,
-        update_in_insert = false,
-      })
-    end
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = false,
+				underline = true,
+				update_in_insert = false,
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright" },
-        handlers = {
-          function(server_name)
-            lspconfig[server_name].setup({})
-          end
-        },
-        automatic_installation = true
-      })
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({})
+					end,
+				},
+				automatic_installation = true,
+			})
 
-      -- leptos lsp config
-      lspconfig.rust_analyzer.setup {
-        -- Other Configs ...
-        settings = {
-          ["rust-analyzer"] = {
-            -- Other Settings ...
-            procMacro = {
-              ignored = {
-                leptos_macro = {
-                  -- optional: --
-                  -- "component",
-                  "server",
-                },
-              },
-            },
-          },
-        }
-      }
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-      }
-    end
-  },
-  { "towolf/vim-helm" },
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.black
-        }
-      })
-    end
-  },
-  {
-    "lervag/vimtex",
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
-    init = function()
-      -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtext_compiler_method = "latexmk"
-      vim.g.vimtex_syntax_enabled = 0
-    end
-  }
+			-- leptos lsp config
+			lspconfig.rust_analyzer.setup({
+				-- Other Configs ...
+				settings = {
+					["rust-analyzer"] = {
+						-- Other Settings ...
+						procMacro = {
+							ignored = {
+								leptos_macro = {
+									-- optional: --
+									-- "component",
+									"server",
+								},
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{ "towolf/vim-helm" },
+	{
+		"nvimtools/none-ls.nvim",
+		enable = false,
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.black,
+				},
+			})
+		end,
+	},
+	{ "mfussenegger/nvim-lint" },
+	{
+		"lervag/vimtex",
+		enable = false,
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtext_compiler_method = "latexmk"
+			vim.g.vimtex_syntax_enabled = 0
+		end,
+	},
 }
