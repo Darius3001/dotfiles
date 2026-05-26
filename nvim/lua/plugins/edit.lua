16923fce4e47501b5a873654988d1f57dac3c7d3
Darius3001 <darius.mertin@outlook.com>
2026-05-26 15:58:00 +0200

    fix

--- nvim/lua/plugins/edit.lua
+++ nvim/lua/plugins/edit.lua
@@ -18,13 +18,13 @@ return {
       })
     end
   },
-  {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    lazy = false,
-    opts = {},
-  },
+  -- {
+  --   "ThePrimeagen/refactoring.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   lazy = false,
+  --   opts = {},
+  -- },
 }
