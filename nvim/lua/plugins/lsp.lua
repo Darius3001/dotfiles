b9a518eb6f460af69df0c6eebbbef774cbf1d897
Darius3001 <darius.mertin@outlook.com>
2026-06-27 12:33:43 +0200

    fix treesitter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -36,14 +36,6 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
 	},
 	{
 		"stevearc/conform.nvim",
